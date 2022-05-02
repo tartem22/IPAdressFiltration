73f136d8c4e7d671b0da77de54686853ed79ff89
tartem22 <tsikunov.aa@yandex.ru>
2022-05-03 01:06:59 +0300

    Добавлен класс-обёртка IpAdress

--- main.cpp
+++ main.cpp
@@ -1,44 +1,41 @@
 #include <iostream>
-#include "lib.h"
+#include "ip_filtration.h"
 
 int main()
 {
 	try
 	{
-		std::vector<std::array<int, 4>> ip_pool;
+		std::vector<IpAdress> ip_pool;
+		ip_pool.reserve(128);
 
 		for (std::string line; std::getline(std::cin, line);)
 		{
 			std::vector<std::string> v = split(line, '\t');
-			std::vector<std::string> parts = split(v.at(0), '.');
-			ip_pool.push_back(std::array<int, 4>{stoi(parts.at(0)),
-												 stoi(parts.at(1)),
-												 stoi(parts.at(2)),
-												 stoi(parts.at(3))});
+			ip_pool.push_back(IpAdress(v.at(0)));
 		}
 
 		sortIPAdresses(ip_pool);
 
 		for (auto ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
-			std::cout << ipArrayToString(*ip) << std::endl;
+			std::cout << ip->toString() << std::endl;
 
-		std::vector<std::array<int, 4>> filtered_1 =
+		std::vector<IpAdress> filtered_1 =
 			filterIPAdresses(ip_pool, 0, 1);
 
 		for (auto ip = filtered_1.cbegin(); ip != filtered_1.cend(); ++ip)
-			std::cout << ipArrayToString(*ip) << std::endl;
+			std::cout << ip->toString() << std::endl;
 
-		std::vector<std::array<int, 4>> filtered_4670 =
+		std::vector<IpAdress> filtered_4670 =
 			filterIPAdresses(filterIPAdresses(ip_pool, 0, 46), 1, 70);
 
 		for (auto ip = filtered_4670.cbegin(); ip != filtered_4670.cend(); ++ip)
-			std::cout << ipArrayToString(*ip) << std::endl;
+			std::cout << ip->toString() << std::endl;
 
-		std::vector<std::array<int, 4>> filtered_46 = 
+		std::vector<IpAdress> filtered_46 = 
 			filterAnyIPAdresses(ip_pool, 46);
 
 		for (auto ip = filtered_46.cbegin(); ip != filtered_46.cend(); ++ip)
-			std::cout << ipArrayToString(*ip) << std::endl;			
+			std::cout << ip->toString() << std::endl;			
 	}
 	catch (const std::exception &e)
 	{
